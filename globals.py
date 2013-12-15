99be618c9cbd2a112a5cf53ee909c7500b0e1884
CALIN Cristian Andrei <cristian.calin@outlook.com>
2013-12-15 00:19:23 +0000

    Separated globals and added wp command for remote interaction via DBus

---
+++ globals.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# These need to be consistent with the gschema.xml file
+APP_NAME = "wallpaper_changer"
+APP_PATH = "/com/kman"
+BASE_ID = "com.kman"
+WALLPAPER_PATH = "path"
+WALLPAPER_INTERVAL = "interval"
+WALLPAPER_SCHEDULE = "schedule"
+
+# Other constants
+TEXT_CONTINUE = "Continue"
+TEXT_PAUSE = "Pause"
+
+# These are gnome specific parameters
+WALLPAPER_SETTING = "org.gnome.desktop.background"
+PICTURE_URI = "picture-uri"
+PICTURE_OPTIONS = "picture-options"
+
