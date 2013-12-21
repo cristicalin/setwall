2c8d9817dc009ca6008ab73a49436a9ea9daebca
CALIN Cristian Andrei <cristian.calin@outlook.com>
2013-12-21 19:33:04 +0000

    fully separated filelist into another class

--- filelist.py
+++ filelist.py
@@ -2,6 +2,7 @@
 
 import os
 import random
+import pyinotify
 
 # File list maintains a randomized list of files in a directory
 # and behaves like a circular list so get_next and get_previous
@@ -10,11 +11,43 @@ import random
 # directory it is monitoriing.
 class filelist:
 
+  # This is a handler used by PyINotify to watch contents of
+  # wallpapers directory path and notify file_list of changes
+  class handlereload(pyinotify.ProcessEvent):
+
+    def __init__(self, list):
+      self.LIST = list
+      
+    def process_IN_CREATE(self, event):
+      if not event.dir:
+        self.LIST.add_file(event.name)
+
+    def process_IN_DELETE(self, event):
+      if not event.dir:
+        self.LIST.remove_file(event.name)
+
   def __init__(self, path):
     self.LOCAL_COUNT = 0
     self.DIR_PATH = path
     self.LOCAL_FILE_LIST = os.listdir(self.DIR_PATH)
-    random.shuffle(self.LOCAL_FILE_LIST)
+
+    # Initialize the notifier and watch the folder for any changes
+    # we only register to the create and delete events
+    # we don't care about other events since they do not impact
+    # the list of files in the directory
+    self.WATCH_MANAGER = pyinotify.WatchManager()
+    self.NOTIFIER = pyinotify.ThreadedNotifier(self.WATCH_MANAGER,
+                                               self.handlereload(self))
+    self.WATCH_MANAGER.add_watch(self.DIR_PATH,
+                      pyinotify.IN_DELETE | pyinotify.IN_CREATE,
+                      rec=False)
+    self.NOTIFIER.start()
+
+  def close(self):
+    # This is to make sure we stop the inotify upon cleanup
+    # unfortuantely Python does not guarantee the call to __del__
+    # so I had to create this miserable hack
+    self.NOTIFIER.stop()
 
   def get_next_file(self):
     self.LOCAL_COUNT += 1
@@ -45,7 +78,13 @@ class filelist:
   def get_list(self):
     return self.LOCAL_FILE_LIST
 
+  def randomize(self):
+    random.shuffle(self.LOCAL_FILE_LIST)
+
+  def sort(self):
+    self.LOCAL_FILE_LIST.sort()
+
 if __name__ == "__main__":
   fl = filelist(".")
   print fl.get_list()
-
+  fl.close()
