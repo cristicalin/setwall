09cbc9bc09159c9517f9f68a8b771c57dc97712a
CALIN Cristian Andrei <cristian.calin@outlook.com>
2014-02-13 19:46:58 +0000

    added GPL v3 License header * this project is licensed under GPL Version 3

--- update_schema.sh
+++ update_schema.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# SetWall - Wallpaper manager
+# 
+# Copyright (C) 2014  Cristian Andrei Calin <cristian.calin@outlook.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 cp com.kman.wallpaper-changer.gschema.xml /usr/share/glib-2.0/schemas/
 chown root:root /usr/share/glib-2.0/schemas/com.kman.wallpaper-changer.gschema.xml
 glib-compile-schemas /usr/share/glib-2.0/schemas/
