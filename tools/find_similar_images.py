b5625273b9762590cdcffaa38fd3958176ac11e4
CALIN Cristian Andrei <cristian.calin@outlook.com>
2021-01-04 14:49:54 +0200

    use cleaner config options

--- tools/find_similar_images.py
+++ tools/find_similar_images.py
@@ -24,7 +24,23 @@ warnings.filterwarnings("ignore")
 
 import multiprocessing
 
-cache = MemcacheClient('localhost', serde=serde.pickle_serde)
+from oslo_config import cfg,types
+
+CONF = cfg.CONF
+
+opts = [
+    cfg.StrOpt("memcache-server", help="Memcached server sddress", default="localhost"),
+    cfg.MultiOpt("path", item_type=types.String(), help="Pictures path(s)", default="."),
+    cfg.IntOpt("threads", help="Number of processing threads", default=2*multiprocessing.cpu_count()),
+    cfg.BoolOpt("delete-bad-files", help="Remove files that fail to process", default=True),
+    cfg.BoolOpt("delete-duplicates", help="Remove duplicate files and keep the one with the higest resolution", default=False),
+    cfg.StrOpt("tombstone", help="Tombstone sufix for deleted duplicates", default=".tombstone")
+]
+
+CONF.register_cli_opts(opts)
+CONF(sys.argv[1:])
+
+cache = MemcacheClient(CONF.memcache_server, serde=serde.pickle_serde)
 img_queue = PriorityQueue()
 hash_queue = PriorityQueue()
 
@@ -35,16 +51,24 @@ STD_PRIO = 100
 HIGH_PRIO = 10
 
 def image_size(img):
-    img_key = hashlib.md5(img.encode('utf-8')).hexdigest()
+    img_key = hashlib.md5(str(img).encode('utf-8')).hexdigest()
     img_hashes = cache.get(img_key)
     if img_hashes:
         return img_hashes.get("size", 0)
     return 0
 
+def is_image(path):
+    suffix = path.suffix.lower()
+    return suffix in [".png", ".jpg", ".jpeg", ".bmp", ".gif", ".jpg", ".svg"]
+
 def get_image_hash():
     while True:
         _prio, img_path, hashfunc = img_queue.get()
-        img_key = hashlib.md5(img_path.encode('utf-8')).hexdigest()
+        if not img_path.exists():
+            # Don't waste cycles for removed images
+            img_queue.task_done()
+            continue
+        img_key = hashlib.md5(str(img_path).encode('utf-8')).hexdigest()
         img_hashes = cache.get(img_key)
         if img_hashes:
             if img_hashes.get(getattr(hashfunc,"__name__")):
@@ -63,9 +87,9 @@ def get_image_hash():
             cache.set(img_key, img_hashes)
             hash_queue.put((STD_PRIO, img_path, hashfunc, img_hash, img_size))
         except Exception as e:
-            print("Removing: '{0}' due to {1}".format(img_path, e))
-            path = Path(img_path)
-            path.unlink(missing_ok=True)
+            if CONF.delete_bad_files:
+                print("Removing: '{0}' due to {1}".format(img_path, e))
+                img_path.unlink(missing_ok=True)
         finally:
             img_queue.task_done()
 
@@ -85,27 +109,32 @@ def check_image_hash():
             cache_item = [img_path]
         cache.set(hash_str, cache_item)
         if len(cache_item) > 1 and hashfunc==SECOND_FUNC:
-            print("Duplicates: {}".format(" ".join(["'{0}'".format(img) for img in sorted(cache_item, key=image_size, reverse=True)])))
+            file_list = sorted(cache_item, key=image_size, reverse=True)
+            print("Duplicates: {}".format(" ".join(["'{0}'".format(img) for img in file_list])))
+            if CONF.delete_duplicates:
+                for path in file_list[1:]:
+                    print(f"Removing {path} duplicate of {file_list[0]}")
+                    path.unlink(missing_ok=True)
+                    tombstone = path.with_suffix(CONF.tombstone)
+                    tombstone.write_text(str(file_list[0]))
         hash_queue.task_done()
 
 def find_similar_images(userpaths):
-    def is_image(filename):
-        f = filename.lower()
-        return f.endswith(".png") or f.endswith(".jpg") or \
-            f.endswith(".jpeg") or f.endswith(".bmp") or \
-            f.endswith(".gif") or '.jpg' in f or  f.endswith(".svg")
-    
     image_filenames = []
     for userpath in userpaths:
-        image_filenames += [os.path.join(userpath, path) for path in os.listdir(userpath) if is_image(path)]
+        image_filenames += [f for f in Path(userpath).iterdir() if f.is_file() and is_image(f)]
     for img in sorted(image_filenames):
         img_queue.put((STD_PRIO, img, MAIN_FUNC))
 
 if __name__ == '__main__':
-    userpaths = sys.argv[1:] if len(sys.argv) > 1 else "."
-    for i in range(2*multiprocessing.cpu_count()):
+    for i in range(CONF.threads):
         Thread(target=get_image_hash, daemon=True).start()
     Thread(target=check_image_hash, daemon=True).start()
-    find_similar_images(userpaths)
-    img_queue.join()
-    hash_queue.join()
+    t = Thread(target=find_similar_images, daemon=False, args=(CONF.path,))
+    t.start()
+    try:
+        t.join()
+        img_queue.join()
+        hash_queue.join()
+    except KeyboardInterrupt:
+        sys.exit()
